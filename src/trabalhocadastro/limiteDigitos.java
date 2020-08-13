09bb2bcccaf57028840b211e6d3fa229d0466a07
Zeni <Zeni@DESKTOP-TVIDDS2>
2020-08-13 19:41:44 -0300

    Primeiro Commit cadastro

---
+++ src/trabalhocadastro/limiteDigitos.java
@@ -0,0 +1,27 @@
+
+package trabalhocadastro;
+
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.PlainDocument;
+
+public class limiteDigitos extends PlainDocument {
+     private int tamanhoMax = 10;
+         
+    public limiteDigitos(int tamanhoMax){
+        this.tamanhoMax = tamanhoMax;
+    }
+    @Override
+    public void insertString(int offset, String str,AttributeSet attr) throws BadLocationException{
+        if (str == null) return;
+        
+        String stringAntiga = getText (0, getLength());
+        int tamanhoNovo = stringAntiga.length() + str.length();
+        
+        if(tamanhoNovo <= tamanhoMax){
+            super.insertString(offset, str, attr);        
+    }else{
+        super.insertString(offset, "", attr);
+    }
+}
+}
